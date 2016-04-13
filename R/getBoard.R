19ab1f6eaded7c263906393238bc09cc79543409
Jakub Chromec <chromec.jakub@gmail.com>
2016-04-13 08:43:38 +0200

    add dateLastActivity

--- R/getBoard.R
+++ R/getBoard.R
@@ -4,6 +4,7 @@
 #' @param path.json path to your JSON file
 #' @param archived whether to export archived cards as well. Defaults to \code{FALSE}
 #' @param comments whether to export comments as well. Defaults to \code{FALSE}
+#' @importFrom RJSONIO fromJSON
 #' @export
 #' @examples
 #'
@@ -18,33 +19,33 @@
 
 getBoard = function(path.json, archived = F, comments = F) {
 
-    require(RJSONIO)
-    data.json = readLines(con = path.json)
-    data.list = fromJSON(data.json)
+   # require(RJSONIO)
+   data.json = readLines(con = path.json)
+   data.list = fromJSON(data.json)
 
-    # EXPORT
-    cards   = getCards(data.list, archived)
-    lists   = getLists(data.list)
-    chlists = getChlists(data.list)
-    members = getMembers(data.list)
+   # EXPORT
+   cards   = getCards(data.list, archived)
+   lists   = getLists(data.list)
+   chlists = getChlists(data.list)
+   members = getMembers(data.list)
 
-    # ADD
-    cards              = cards[order(cards$name), ]    # order cards or they won't match with list.name vector from addLists
-    cards$list.id      = addLists(cards, lists)
-    cards              = cards[order(cards$list.id), ] # order them back to list name
-    cards$member.id    = addMembers(cards, members)
-    cards$checklist.id = addChlists(cards, chlists)
+   # ADD
+   cards              = cards[order(cards$name), ]    # order cards or they won't match with list.name vector from addLists
+   cards$list.id      = addLists(cards, lists)
+   cards              = cards[order(cards$list.id), ] # order them back to list name
+   cards$member.id    = addMembers(cards, members)
+   cards$checklist.id = addChlists(cards, chlists)
 
-    # Change names
-    names(cards)[c(1, 7, 10)] = c("list", "checklists", "members")
+   # Change names
+   names(cards)[c(1, 7, 10, 11)] = c("list", "checklists", "activity", "members")
 
-    # if comments == T, add comments as well
-    if (comments) cards$comments = addComments(cards, data.list)
+   # if comments == T, add comments as well
+   if (comments) cards$comments = addComments(cards, data.list)
 
-    # remove auxiliary column
-    cards$card.id = NULL
+   # remove auxiliary column
+   cards$card.id = NULL
 
-    return(cards)
+   return(cards)
 
 }
 
@@ -52,178 +53,182 @@ getBoard = function(path.json, archived = F, comments = F) {
 
 getCards = function(data.list, archived = F) {
 
-    # Subset cards
-    if (archived) cards = data.list$cards else {
-        subset.cards = sapply(data.list$cards, function(x) { x$closed == F } )
-        cards = data.list$cards[subset.cards]
-    }
+   # Subset cards
+   if (archived) cards = data.list$cards else {
+      subset.cards = sapply(data.list$cards, function(x) { x$closed == F } )
+      cards = data.list$cards[subset.cards]
+   }
 
-    x = sapply(cards, function(x) {
+   x = sapply(cards, function(x) {
 
-        # check whether everything optional is present; if not, set to ""
-        if (is.null(x$due) == T) { x$due = "" }
-        if (is.null(x$idMembers) == T) { x$idMembers = "" }
+      # check whether everything optional is present; if not, set to ""
+      if (is.null(x$due) == T) { x$due = "" }
+      if (is.null(x$idMembers) == T) { x$idMembers = "" }
 
-        # get label names:
-        lab.names = sapply(x$labels, "[[", "name")
-        lab.names = paste(lab.names, collapse = ", ")
+      # get label names:
+      lab.names = sapply(x$labels, "[[", "name")
+      lab.names = paste(lab.names, collapse = ", ")
 
-        # get checklist IDs
-        chlist = paste(x$idChecklists, collapse = "\n")
+      # get checklist IDs
+      chlist = paste(x$idChecklists, collapse = "\n")
 
-        # get attachments - names plus links
-        att.names = sapply(x$attachments, "[[", "name")
-        att.links = sapply(x$attachments, "[[", "url")
-        att.links = paste("(", att.links, ")", sep = "")
-        atts = paste(att.names, att.links, collapse = "\n")
-        atts = sub(" \\(\\)", "", atts) # remove brackets from empty elements
+      # get attachments - names plus links
+      att.names = sapply(x$attachments, "[[", "name")
+      att.links = sapply(x$attachments, "[[", "url")
+      att.links = paste("(", att.links, ")", sep = "")
+      atts = paste(att.names, att.links, collapse = "\n")
+      atts = sub(" \\(\\)", "", atts) # remove brackets from empty elements
 
-        # get members
-        membs = paste(x$idMembers, collapse = ", ")
+      # get members
+      membs = paste(x$idMembers, collapse = ", ")
 
-        # combine everything together
-        x = c(x$idList, x$idShort, x$name, x$closed, x$shortUrl, x$desc, chlist,
-              atts, x$due, membs, lab.names, x$id)
-    })
+      # combine everything together
+      x = c(x$idList, x$idShort, x$name, x$closed, x$shortUrl, x$desc, chlist,
+            atts, x$due, x$dateLastActivity, membs, lab.names, x$id)
+   })
 
-    cards = data.frame(t(x), stringsAsFactors = F)
-    names(cards) = c("list.id", "card#", "name", "archived", "url", "description",
-                     "checklist.id", "attachments", "duedate", "member.id", "labels",
-                     "card.id")
-    return(cards)
+   cards = data.frame(t(x), stringsAsFactors = F)
+   names(cards) = c("list.id", "card#", "name", "archived", "url", "description", "checklist.id", "attachments", "duedate", "last.activity", "member.id", "labels", "card.id")
+
+   # Convert columns with date strings to proper datetime format
+   cards$duedate = strptime(cards$duedate, "%FT%T")
+   cards$last.activity = strptime(cards$last.activity, "%FT%T")
+
+   return(cards)
 }
 
 getChlists = function(data.list) {
 
-    item.text = sapply(data.list$checklists, function(x) {
+   item.text = sapply(data.list$checklists, function(x) {
 
-        y = sapply(x$checkItems, function(y) {
+      y = sapply(x$checkItems, function(y) {
 
-            y = c(y$state, y$name)
-            y = paste(y, collapse = ": ")
-            return(y)
+         y = c(y$state, y$name)
+         y = paste(y, collapse = ": ")
+         return(y)
 
-        })
+      })
 
-        y = paste(y, collapse = "\n")
-        y = paste(x$name, y, sep = "\n")
-        return(y)
+      y = paste(y, collapse = "\n")
+      y = paste(x$name, y, sep = "\n")
+      return(y)
 
-    })
+   })
 
-    checklist.id = sapply(data.list$checklists, "[[", "id")
-    chlists = data.frame(checklist.id, item.text)
+   checklist.id = sapply(data.list$checklists, "[[", "id")
+   chlists = data.frame(checklist.id, item.text)
 
-    return(chlists)
+   return(chlists)
 
 }
 
 addChlists = function(cards, chlists) {
 
-    checklists = sapply(cards$checklist.id, function(x) {
+   checklists = sapply(cards$checklist.id, function(x) {
 
-        # break down multiple IDs within one cell
-        x = unlist(strsplit(as.character(x), "\n"))
+      # break down multiple IDs within one cell
+      x = unlist(strsplit(as.character(x), "\n"))
 
-        # merge with member list
-        x = merge(chlists, x, by = 1)
-        x = x$item.text
+      # merge with member list
+      x = merge(chlists, x, by = 1)
+      x = x$item.text
 
-        # collapse back into one cell
-        x = paste(x, collapse = "\n\n")
-        return(x)
-    })
+      # collapse back into one cell
+      x = paste(x, collapse = "\n\n")
+      return(x)
+   })
 
-    return(checklists)
+   return(checklists)
 }
 
 getMembers = function(data.list) {
 
-    x = sapply(data.list$members, function(x) { x = c(x$id, x$fullName) })
-    members = data.frame(t(x))
-    names(members) = c("id", "full.name")
+   x = sapply(data.list$members, function(x) { x = c(x$id, x$fullName) })
+   members = data.frame(t(x))
+   names(members) = c("id", "full.name")
 
-    return(members)
+   return(members)
 
 }
 
 addMembers = function(cards, members) {
 
-    x = sapply(cards$member.id, function(x) {
+   x = sapply(cards$member.id, function(x) {
 
-        # break down multiple IDs within one cell
-        x = as.character(x)
-        x = unlist(strsplit(x, ", "))
+      # break down multiple IDs within one cell
+      x = as.character(x)
+      x = unlist(strsplit(x, ", "))
 
-        # merge with member list
-        x = merge(members, x, by = 1)
-        x = x$full.name
+      # merge with member list
+      x = merge(members, x, by = 1)
+      x = x$full.name
 
-        # collapse back into one cell
-        x = paste(x, collapse = ", ")
+      # collapse back into one cell
+      x = paste(x, collapse = ", ")
 
-        return(x)
+      return(x)
 
-    })
+   })
 }
 
 getLists = function(data.list) {
 
-    x = sapply(data.list$lists, function(x) { x = c(x$name, x$id) })
-    lists = data.frame(t(x), stringsAsFactors = F)
-    names(lists) = c("list.name", "list.id")
+   x = sapply(data.list$lists, function(x) { x = c(x$name, x$id) })
+   lists = data.frame(t(x), stringsAsFactors = F)
+   names(lists) = c("list.name", "list.id")
 
-    return(lists)
+   return(lists)
 
 }
 
 addLists = function(cards, lists) {
 
-    x = merge(cards, lists, by = "list.id", all.x = T)
-    x = x[order(x$name), ]
-    x = x$list.name
+   x = merge(cards, lists, by = "list.id", all.x = T)
+   x = x[order(x$name), ]
+   x = x$list.name
 
-    return(x)
+   return(x)
 }
 
 addComments = function(cards, data.list) {
 
-    # subset actions
-    subset = sapply(data.list$actions, function(x) {x$type == "commentCard"} )
-    comments = data.list$actions[subset]
+   # subset actions
+   subset = sapply(data.list$actions, function(x) {x$type == "commentCard"} )
+   comments = data.list$actions[subset]
 
-    # get id, date, member, text
-    comments = sapply(comments, function(x) {
+   # get id, date, member, text
+   comments = sapply(comments, function(x) {
 
-        id = x$data$card$id
-        item = paste("[", x$date, " - ", x$memberCreator[["username"]], "] ",
-                     x$data$text,
-                     collapse = "")
-        comment = c(id, item)
-        return(comment)
+      id = x$data$card$id
+      item = paste("[", x$date, " - ", x$memberCreator[["username"]], "] ",
+                   x$data$text,
+                   collapse = "")
+      comment = c(id, item)
+      return(comment)
 
-    })
+   })
 
-    comments = data.frame(t(comments))
-    names(comments) = c("card.id", "item")
+   comments = data.frame(t(comments))
+   names(comments) = c("card.id", "item")
 
-    y = by(comments, comments$card.id, function (x) {
+   y = by(comments, comments$card.id, function (x) {
 
-        a = unique(as.character(x[,1]))
-        b = paste(as.character(x[,2]), collapse = "\n")
-        c = c(a, b)
+      a = unique(as.character(x[,1]))
+      b = paste(as.character(x[,2]), collapse = "\n")
+      c = c(a, b)
 
-        return(c)
-    })
+      return(c)
+   })
 
-    x = sapply(y, function(x) { out = c(x[1], x[2]) })
-    comments = data.frame(t(x), row.names = NULL)
-    names(comments) = c("card.id", "comments")
+   x = sapply(y, function(x) { out = c(x[1], x[2]) })
+   comments = data.frame(t(x), row.names = NULL)
+   names(comments) = c("card.id", "comments")
 
-    # Add COMMENTS to the CARDS table
-    x = merge(cards, comments, by = "card.id", all.x = T)
-    x = x$comments
+   # Add COMMENTS to the CARDS table
+   x = merge(cards, comments, by = "card.id", all.x = T)
+   x = x$comments
 
-    return(x)
+   return(x)
 
 }
+
