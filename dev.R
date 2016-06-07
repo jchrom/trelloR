6d1fa04f7ac77bbdd6b4eaa4943f5183ab590c32
Jakub Chromec <chromec.jakub@gmail.com>
2016-06-07 08:35:09 +0200

    .

--- dev.R
+++ dev.R
@@ -6,5 +6,14 @@
 #    - included by default: labels
 #    - optional chocies (logical): archived, members, comments
 
+library(httr)
+library(jsonlite)
+
 source("keys/keys.R")
 token = get_token(key, secret)
+
+url  = "https://trello.com/b/4rm75Nol/evidence-kurzu.json"
+req  = GET(url, config(token = token))
+json = content(req, as = "text")
+board = fromJSON(json)
+
