6d22fef76260e434414cd4386f51ba86e808bf7b
Jakub Chromec <chromec.jakub@gmail.com>
2016-06-07 15:30:13 +0200

    .

--- R/get_board.R
+++ R/get_board.R
@@ -11,7 +11,7 @@
 #' source("mykeys.R")
 #' token = get_token(key, secret)
 #'
-#' You will be prompted to confirm the authorization in a browser. You will also be offered an option to store the authentication in your working directory, in a hidden '.httr-oauth' file (do NOT share it with anyone!).
+#' # You will be prompted to confirm the authorization in a browser. You will also be offered an option to store the authentication in your working directory, in a hidden '.httr-oauth' file (do NOT share it with anyone!).
 
 get_board = function(url, token = NULL) {
 
