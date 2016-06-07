d1cfdc8d751e65dd0c04e4c88e0e25f75e6a9e2e
Jakub Chromec <chromec.jakub@gmail.com>
2016-06-07 15:30:04 +0200

    .

--- dev.R
+++ dev.R
@@ -6,6 +6,11 @@
 #    - included by default: labels
 #    - optional chocies (logical): archived, members, comments
 
+# 3. More functions
+#    - organization-centered overview: teams, boards, members
+#    - member-centered overview: teams, boards, cards
+#    - board-centered overview: members, cards
+
 library(httr)
 library(jsonlite)
 # library(dplyr)
@@ -20,3 +25,12 @@ board = get_board(url, token)
 
 # Get cards
 cards = board$cards
+
+# List member's boards
+call = "https://api.trello.com/1/members/jakubchromec/"
+what = "boards"
+call_req = paste0(call, what, "?key=", key)
+
+req   = GET(call_req, config(token = token))
+json  = content(req, as = "text")
+data = fromJSON(json)
