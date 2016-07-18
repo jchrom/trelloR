f36018a1d5b1fadc576bb147a65a6ceadd652dbe
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-18 18:33:34 +0200

    generic function to get data via Trello API

---
+++ R/get_board_requests.R
@@ -0,0 +1,106 @@
+#' Get All Trello Board Cards
+#'
+#' Returns a flat \code{data.frame} with all cards from a given Trello board.
+#' @param boardid id of the desired board
+#' @param token previously generated token (see ?get_token for help)
+#' @export
+#' @examples
+#' cards = get_board_cards(url, token)
+
+get_board_cards = function(boardid, token) {
+
+    # Get data
+    cards = get_request("board", boardid, "cards", token)
+
+    # Tidy up a bit
+    cards = cards %>%
+        select(
+            card_name = name,
+            card_id = id,
+            card_url = url,
+            labels,
+            card_desc = desc,
+            card_arch = closed,
+            card_url_short = shortUrl,
+            last_act = dateLastActivity,
+            due_date = due,
+            board_id = idBoard,
+            list_id = idList,
+            checklist_id = idChecklists,
+            member_id = idMembers)
+
+    return(cards)
+}
+
+#' Get All Trello Board Lists
+#'
+#' Returns a flat \code{data.frame} with all lists from a given Trello board.
+#' @param boardid id of the desired board
+#' @param token previously generated token (see ?get_token for help)
+#' @export
+#' @examples
+#' lists = get_board_lists(url, token)
+
+get_board_lists = function(boardid, token) {
+
+    # Get data
+    lists = get_request("board", boardid, "lists", token)
+
+    # Tidy up a bit
+    lists = lists %>%
+        select(
+            list_id = id,
+            list_name = name,
+            list_arch = closed,
+            board_id = idBoard)
+
+    return(lists)
+}
+
+#' Get All Trello Board Memebers
+#'
+#' Returns a flat \code{data.frame} with all members from a given Trello board.
+#' @param boardid id of the desired board
+#' @param token previously generated token (see ?get_token for help)
+#' @export
+#' @examples
+#' lists = get_board_members(url, token)
+
+get_board_members = function(boardid, token) {
+
+    # Get data
+    members = get_request("board", boardid, "members", token)
+
+    # Tidy up a bit
+    members = members %>%
+        select(
+            member_id = id,
+            member_name = fullName,
+            member_uname = username)
+
+    return(members)
+}
+
+#' Get All Trello Board Labels
+#'
+#' Returns a flat \code{data.frame} with all labels from a given Trello board.
+#' @param boardid id of the desired board
+#' @param token previously generated token (see ?get_token for help)
+#' @export
+#' @examples
+#' lists = get_board_members(url, token)
+
+get_board_labels = function(boardid, token) {
+
+    # Get data
+    labels = get_request("board", boardid, "labels", token)
+
+    # Tidy up a bit
+    labels = labels %>%
+        select(
+            label_id = id,
+            label_name = name,
+            label_color = color)
+
+    return(labels)
+}
