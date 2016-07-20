fadc3ab561f171a78aca6e18147259baef2eb45f
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-20 15:26:13 +0200

    .

--- dev2.R
+++ dev2.R
@@ -1,11 +1,15 @@
-# Libs
+# To do ----
+# 1. Pagination
+# 2. 'Raw' option for all functions (=response as flat data.frame without tidying)
+
+# Libs ----
 library(httr)
 library(jsonlite)
 library(extr)
 library(dplyr)
 library(tidyr)
 
-# Get token and data
+# Get token and data ----
 source(file.choose())
 token = get_token(key, secret)
 
@@ -13,6 +17,16 @@ token = get_token(key, secret)
 url = "https://trello.com/b/QnY5i1l7/av-asistence.json"
 brd = get_board(url, token)
 
+# Get boards
+my_b = get_my_boards(token)
+id_b = my_b$board_id[1]
+
 # Get cards
-cards = brd$cards
+crd = get_board_cards(id_b, token)
+
+
+
+
+
+
 
