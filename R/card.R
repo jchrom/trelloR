b82183662b0d96f1a08459f5356f918fbca02403
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-23 22:24:53 +0200

    .

--- R/card.R
+++ R/card.R
@@ -3,12 +3,16 @@
 #' Returns a flat \code{data.frame} with all comments from a given Trello card.
 #' @param id id of the desired card
 #' @param token previously generated token, see \code{\link{get_token}} for info on how to obtain it
+#' @param paginate logical whether to use pagination (for requests returning more than 1000 rows). Defaults to \code{FALSE}
+#' @param simplify logical drop and rename some columns to make the result simpler or leave it as it is. Defaults to \code{TRUE}
 #' @export
 #' @examples
 #' comments = get_card_comments(cardid, token)
 
 get_card_comments = function(id,
-                             token) {
+                             token,
+                             paginate = FALSE,
+                             simplify = TRUE) {
 
     # Build url and query
     url   = paste0("https://api.trello.com/1/cards/", id, "/actions")
@@ -19,13 +23,14 @@ get_card_comments = function(id,
                            paginate = paginate)
 
     # Tidy up a bit
-    comments$date = as.POSIXct(strptime(comments$date, "%Y-%m-%dT%H:%M:%OS"))
-    comments = comments %>%
-        select(
-            comment = data.text,
-            comment_date = date,
-            card_id = data.card.id,
-            member_name = memberCreator.fullName)
-
+    if (simplify) {
+        comments$date = as.POSIXct(strptime(comments$date, "%Y-%m-%dT%H:%M:%OS"))
+        comments = comments %>%
+            select(
+                comment = data.text,
+                comment_date = date,
+                card_id = data.card.id,
+                member_name = memberCreator.fullName)
+    }
     return(comments)
 }
