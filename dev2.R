9bc19383ee78c3cb07bdb19199ff9337ed54c7d0
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-23 22:40:31 +0200

    .

--- dev2.R
+++ dev2.R
@@ -1,8 +1,11 @@
 # To do ----
 # 1. [DONE] Pagination
 # 2. [DONE] simplify = FALSE option for all functions (=response as flat data.frame without tidying)
-# 3. []     Proper error handling for get_flat (use the request headers etc. to get the code and its meaning)
-#    []     Build query using the key-value pairs as described in https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
+# 3. [DONE] Proper error handling for get_flat (use the request headers etc. to get the code and its meaning)
+#    [DONE] Build query using the key-value pairs as described in https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
+# 4. []     Add get_card_ functions (and tests)
+# 5. []     Update intro
+# 6. []     Make a list of things necessary to submit to CRAN
 
 # Libs ----
 library(httr)
