e9fc36dc427aba30edd1edde1e18b5b5908b6d93
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-23 22:18:46 +0200

    .

--- R/card.R
+++ R/card.R
@@ -1,16 +1,22 @@
 #' Get All Trello Card Comments
 #'
 #' Returns a flat \code{data.frame} with all comments from a given Trello card.
-#' @param boardid id of the desired board
-#' @param token previously generated token (see ?get_token for help)
+#' @param id id of the desired card
+#' @param token previously generated token, see \code{\link{get_token}} for info on how to obtain it
 #' @export
 #' @examples
 #' comments = get_card_comments(cardid, token)
 
-get_card_comments = function(cardid, token) {
+get_card_comments = function(id,
+                             token) {
+
+    # Build url and query
+    url   = paste0("https://api.trello.com/1/cards/", id, "/actions")
+    query = list(limit = "1000", filter = "commentCard")
 
     # Get data
-    comments = get_request("card", cardid, "actions?filter=commentCard", token)
+    comments = get_request(url = url, token = token, query = query,
+                           paginate = paginate)
 
     # Tidy up a bit
     comments$date = as.POSIXct(strptime(comments$date, "%Y-%m-%dT%H:%M:%OS"))
