b8796d8056aa4b45708c8fddc86a3820f717f3ba
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-25 13:49:37 +0200

    .

--- R/get_token.R
+++ R/get_token.R
@@ -1,9 +1,12 @@
 #' Authorize API access to Trello
 #'
-#' Token is needed in order to get data directly from Trello via API. To create it, get your 'key' and 'secret' here: https://trello.com/app-key.
-#' @param key key
-#' @param secret secret
+#' Creates a secure token to communicate with Trello API. You will need your
+#' developer 'key' and 'secret' - get them here:
+#' \url{https://trello.com/app-key}.
+#' @param key developer key as character string
+#' @param secret developer secret as character string
 #' @param app optional app name, defaults to "trello"
+#' @seealso \code{\link[httr]{oauth_app}}, \code{\link[httr]{oauth_endpoint}}, \code{\link[httr]{oauth1.0_token}}
 #' @importFrom httr oauth_app oauth_endpoint oauth1.0_token
 #' @export
 #' @examples
@@ -19,7 +22,7 @@ get_token = function(key, secret, app = "trello") {
 
     # 1. Create an app
     trello.app = oauth_app(
-        app,
+        app = app,
         key = key,
         secret = secret)
 
