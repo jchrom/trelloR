f2d92a980806eef3689a2617a9273f29407f54d7
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-25 13:49:34 +0200

    .

--- R/card.R
+++ R/card.R
@@ -1,13 +1,13 @@
-#' Get All Trello Card Comments
+#' Get Card Comments
 #'
-#' Returns a flat \code{data.frame} with all comments from a given Trello card.
+#' Given a card ID, returns a flat \code{data.frame} with comments-related data.
 #' @param id id of the desired card
-#' @param token previously generated token, see \code{\link{get_token}} for info on how to obtain it
-#' @param paginate logical whether to use pagination (for requests returning more than 1000 rows). Defaults to \code{FALSE}
-#' @param simplify logical drop and rename some columns to make the result simpler or leave it as it is. Defaults to \code{TRUE}
+#' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
+#' @param paginate whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
+#' @param simplify drop and rename some columns or leave it as is. Defaults to \code{TRUE}
 #' @export
 #' @examples
-#' comments = get_card_comments(cardid, token)
+#' comments = get_card_comments(id, token)
 
 get_card_comments = function(id,
                              token,
@@ -23,14 +23,74 @@ get_card_comments = function(id,
                            paginate = paginate)
 
     # Tidy up a bit
-    if (simplify) {
-        comments$date = as.POSIXct(strptime(comments$date, "%Y-%m-%dT%H:%M:%OS"))
-        comments = comments %>%
-            select(
-                comment = data.text,
-                comment_date = date,
-                card_id = data.card.id,
-                member_name = memberCreator.fullName)
-    }
+    if (simplify) comments = simplify_comments(comments)
+
+    # Return result
     return(comments)
+}
+
+#' Get Card Actions
+#'
+#' Given a card ID, returns a flat \code{data.frame} with actions-related data.
+#' @param id id of the desired card
+#' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
+#' @param paginate whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
+#' @param simplify drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
+#' @importFrom dplyr %>% select mutate matches everything
+#' @export
+#' @examples
+#' actions = get_card_actions(id, token)
+
+get_card_actions = function(id,
+                            token,
+                            paginate = FALSE,
+                            simplify = TRUE) {
+
+    # Build url & query
+    url = paste0("https://api.trello.com/1/card/", id, "/actions")
+
+    # Get data
+    actions = get_trello(url = url, token = token,
+                         paginate = paginate)
+
+    # Tidy up a bit
+    if (simplify) actions = simplify_actions(actions)
+
+    # Return result
+    return(actions)
+}
+
+#' Get Card Members
+#'
+#' Given a card ID, returns a flat \code{data.frame} with members-related data.
+#' @param id id of the desired board
+#' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
+#' @param paginate whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
+#' @param simplify drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
+#' @importFrom dplyr %>% select
+#' @export
+#' @examples
+#' members = get_board_members(id, token)
+
+get_card_members = function(id,
+                            token,
+                            paginate = FALSE,
+                            simplify = TRUE) {
+
+    # Build url & query
+    url = paste0("https://api.trello.com/1/card/", id, "/members")
+
+    # Get data
+    members = get_trello(url = url, token = token,
+                         paginate = paginate)
+
+    # Tidy up a bit
+    if (simplify) {
+        members = members %>%
+            select(
+                member_id = id,
+                member_name = fullName,
+                member_uname = username)
+    }
+    return(members)
 }
