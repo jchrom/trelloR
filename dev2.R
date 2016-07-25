9dd7f615d22177d27011c1535777b50db593eaf9
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-25 13:49:15 +0200

    .

--- dev2.R
+++ dev2.R
@@ -1,11 +1,16 @@
 # To do ----
-# 1. [DONE] Pagination
-# 2. [DONE] simplify = FALSE option for all functions (=response as flat data.frame without tidying)
-# 3. [DONE] Proper error handling for get_flat (use the request headers etc. to get the code and its meaning)
-#    [DONE] Build query using the key-value pairs as described in https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
-# 4. []     Add get_card_ functions (and tests)
-# 5. []     Update intro
-# 6. []     Make a list of things necessary to submit to CRAN
+# [DONE] Pagination
+# [DONE] simplify = FALSE option for all functions (=response as flat data.frame without tidying)
+# [DONE] Proper error handling for get_flat (use the request headers etc. to get the code and its meaning)
+# [DONE] Build query using the key-value pairs as described in https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
+# [DONE] Add handling of empty results (= 0 rows)
+# [DONE] Add actions if they exist (such as "data.attachment.url")
+# [DONE] get_board_ and get_cards do the same simplification, come up with a solution
+# [20% ] ALL the equivalent functions now share the same simplifying function
+# [80% ]     Add get_card_ functions
+# []     Add tests
+# []     Update intro
+# []     Make a list of things necessary to submit to CRAN
 
 # Libs ----
 # library(httr)
@@ -16,20 +21,20 @@ library(trello)
 
 # Get token and data ----
 source(file.choose())
-token = get_token(key, secret, app = "trello")
+token = get_token(key, secret, app = "trello-kancl")
 
 # get_my_boards ----
-my_b = get_my_boards(token)
-id_b = my_b$board_id[1]
+mb = get_my_boards(token)
+id = mb$board_id[1]
 
 # get_board_ ----
-crd      = get_board_cards(id_b, token, filter = "open")
-act_1000 = get_board_actions(id_b, token)
-act_all  = get_board_actions(id_b, token, paginate = TRUE)
-lists    = get_board_lists(id_b, token)
-members  = get_board_members(id_b, token)
-labels   = get_board_labels(id_b, token)
-comments = get_board_comments(id_b, token)
+crds     = get_board_cards(id, token, filter = "open")
+act_1000 = get_board_actions(id, token)
+act_all  = get_board_actions(id, token, paginate = TRUE)
+lists    = get_board_lists(id, token)
+members  = get_board_members(id, token)
+labels   = get_board_labels(id, token)
+comments = get_board_comments(id, token)
 
 # get_card_ ----
 crd_comments = get_card_comments(crd$card_id[18], token)
