d6cdf83c6d4577b21eacb0b5eb7b13010959d6b2
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-26 11:16:02 +0200

    place for all the simplify_ functions

---
+++ R/simplify.R
@@ -0,0 +1,60 @@
+simplify_actions = function(actions) {
+
+    # Check for optional columns and rename
+    actions = actions %>%
+        mutate(
+            comment_text    = if (exists('data.text', where = actions)) {
+                data.text } else { NA },
+            item_update     = if (exists('data.checkItem.state', where = actions)) {
+                data.checkItem.state } else { NA },
+            item_name       = if (exists('data.checkItem.name', where = actions)) {
+                data.checkItem.name } else { NA },
+            checklist_name  = if (exists('data.checklist.name', where = actions)) {
+                data.checklist.name } else { NA },
+            attachment_name = if (exists('data.attachment.name', where = actions)) {
+                data.attachment.name } else { NA },
+            attachment_url  = if (exists('data.attachment.url', where = actions)) {
+                data.attachment.url } else { NA })
+
+    # Format date
+    actions = actions %>%
+        mutate(
+            action_created  = as_POSIXct_hex(strtrim(id, 8)))
+
+    # Rename/select
+    actions = actions %>%
+        select(
+            action_type = type, action_created,
+            member_id   = idMemberCreator,
+            board_id    = data.board.id,
+            card_id     = data.card.id,
+            list_id        = data.list.id,
+            list_before_id = data.listBefore.id,
+            list_after_id  = data.listAfter.id,
+            comment_text,
+            attachment_name, attachment_url,
+            item_update, item_name, checklist_name)
+
+    # Drop columns with only NAs
+    actions = Filter(function(x) any(!is.na(x)), actions)
+
+    # Return the result
+    return(actions)
+}
+
+simplify_comments = function(comments) {
+
+    # Format date and rename/select columns
+    comments = comments %>%
+        mutate(
+            comment_created = as_POSIXct_hex(strtrim(id, 8))) %>%
+        select(
+            comment = data.text,
+            comment_created,
+            card_id = data.card.id,
+            member_name = memberCreator.fullName,
+            member_init = memberCreator.initials)
+
+    # Return result
+    return(comments)
+}
