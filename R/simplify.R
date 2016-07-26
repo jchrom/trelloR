a35af4f3b7d9567b5aa1dbf115c0ca3293d847d3
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-26 12:45:46 +0200

    .

--- R/simplify.R
+++ R/simplify.R
@@ -49,12 +49,76 @@ simplify_comments = function(comments) {
         mutate(
             comment_created = as_POSIXct_hex(strtrim(id, 8))) %>%
         select(
-            comment = data.text,
+            comment_id      = id,
             comment_created,
-            card_id = data.card.id,
-            member_name = memberCreator.fullName,
-            member_init = memberCreator.initials)
+            member_name     = memberCreator.fullName,
+            member_init     = memberCreator.initials,
+            card_id         = data.card.id,
+            card_name       = data.card.name,
+            comment_created = date,
+            comment_last_ed = data.dateLastEdited,
+            comment_text    = data.text)
 
     # Return result
     return(comments)
+}
+
+simplify_members = function(members) {
+
+    # Rename/select columns
+    members = members %>%
+        select(
+            member_id = id,
+            member_name = fullName,
+            member_uname = username)
+
+    # Return result
+    return(members)
+
+}
+
+simplify_lists = function(lists) {
+
+    # Rename/select columns
+    lists = lists %>%
+        select(
+            list_id = id,
+            list_name = name,
+            list_arch = closed,
+            board_id = idBoard)
+
+    # Return result
+    return(lists)
+}
+
+simplify_cards = function(cards) {
+
+    # Format date
+    cards = cards %>%
+        mutate(
+            card_created  = as_POSIXct_hex(strtrim(id, 8)))
+
+    # Rename/select columns
+    cards = cards %>%
+        select(
+            card_name = name,
+            card_id = id,
+            card_url = url,
+            card_labels = labels,
+            card_desc = desc,
+            card_arch = closed,
+            card_url_short = shortUrl,
+            card_last_act = dateLastActivity,
+            card_due = due,
+            card_created,
+            # label_color = ,
+            # label_name = ,
+            board_id = idBoard,
+            list_id = idList,
+            checklist_id = idChecklists,
+            member_id = idMembers)
+
+    # Return result
+    return(cards)
+
 }
