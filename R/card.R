d7768f2315e307ac70adbb34484a05aa779c0d49
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-26 13:00:13 +0200

    add @seealso

--- R/card.R
+++ R/card.R
@@ -5,6 +5,7 @@
 #' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
 #' @param paginate whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
 #' @param simplify drop and rename some columns or leave it as is. Defaults to \code{TRUE}
+#' @seealso \code{\link{get_token}}, \code{\link{get_trello}}
 #' @export
 #' @examples
 #' comments = get_card_comments(id, token)
@@ -36,6 +37,7 @@ get_card_comments = function(id,
 #' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
 #' @param paginate whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
 #' @param simplify drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
+#' @seealso \code{\link{get_token}}, \code{\link{get_trello}}
 #' @importFrom dplyr %>% select mutate matches everything
 #' @export
 #' @examples
@@ -67,6 +69,7 @@ get_card_actions = function(id,
 #' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
 #' @param paginate whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
 #' @param simplify drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
+#' @seealso \code{\link{get_token}}, \code{\link{get_trello}}
 #' @importFrom dplyr %>% select
 #' @export
 #' @examples
