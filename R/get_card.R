c000c6a77277dbfe43bf34b61e0007adee27bd59
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-27 18:17:45 +0200

    .

--- R/get_card.R
+++ R/get_card.R
@@ -4,11 +4,11 @@
 #'
 #' Given a card ID, returns a flat \code{data.frame} with actions-related data.
 #' @param id card id
-#' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
+#' @param token previously generated token, see \code{\link{trello_get_token}} for how to obtain it
 #' @param filter limit to just one type of action, e.g. "addMemberToCard"
 #' @param paging whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
 #' @param fix drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
-#' @seealso \code{\link{trello_get_token}}, \code{\link{trello_trello}}
+#' @seealso \code{\link{trello_get_token}}, \code{\link{trello_get}}
 #' @export
 
 get_card_actions = function(id, token,
@@ -28,7 +28,7 @@ get_card_actions = function(id, token,
 #'
 #' Given a card ID, returns a flat \code{data.frame} with comments-related data.
 #' @param id card id
-#' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
+#' @param token previously generated token, see \code{\link{trello_get_token}} for how to obtain it
 #' @param paging whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
 #' @param fix drop and rename some columns or leave it as is. Defaults to \code{TRUE}
 #' @seealso \code{\link{trello_get_token}}, \code{\link{trello_get}}
@@ -50,7 +50,7 @@ get_card_comments = function(id, token,
 #'
 #' Given a card ID, returns a flat \code{data.frame} with members-related data.
 #' @param id card id
-#' @param token previously generated token, see \code{\link{get_token}} for how to obtain it
+#' @param token previously generated token, see \code{\link{trello_get_token}} for how to obtain it
 #' @param paging whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
 #' @param fix drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
 #' @seealso \code{\link{trello_get_token}}, \code{\link{trello_get}}
