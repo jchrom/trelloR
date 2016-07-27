f12a9370fdb4ff5b247fc7f91ff49526418f87cb
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-27 18:17:42 +0200

    .

--- R/get_board.R
+++ R/get_board.R
@@ -96,10 +96,10 @@ get_board_members = function(id, token,
 #'
 #' Given a board ID, returns a flat \code{data.frame} with labels-related data.
 #' @param id board id
-#' @param token previously generated token, see \code{\link{get_token}} for info on how to obtain it
+#' @param token previously generated token, see \code{\link{trello_get_token}} for info on how to obtain it
 #' @param paging whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
 #' @param fix drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
-#' @seealso \code{\link{get_token}}, \code{\link{trello_get}}
+#' @seealso \code{\link{trello_get_token}}, \code{\link{trello_get}}
 #' @export
 
 get_board_labels = function(id, token,
@@ -118,10 +118,10 @@ get_board_labels = function(id, token,
 #'
 #' Given a board ID, returns a flat \code{data.frame} with labels-related data.
 #' @param id board id
-#' @param token previously generated token, see \code{\link{get_token}} for info on how to obtain it
+#' @param token previously generated token, see \code{\link{trello_get_token}} for info on how to obtain it
 #' @param paging whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
 #' @param fix drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
-#' @seealso \code{\link{get_token}}, \code{\link{trello_get}}
+#' @seealso \code{\link{trello_get_token}}, \code{\link{trello_get}}
 #' @export
 
 get_board_comments = function(id, token,
