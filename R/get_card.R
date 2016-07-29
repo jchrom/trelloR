3a20db608956b6f247b6847527ec30f05cfd1c63
Jakub Chromec <chromec.jakub@gmail.com>
2016-07-29 22:40:06 +0200

    use ... to pass args to trello_get

--- R/get_card.R
+++ R/get_card.R
@@ -4,22 +4,14 @@
 #'
 #' Given a card ID, returns a flat \code{data.frame} with actions-related data.
 #' @param id card id
-#' @param token previously generated token, see \code{\link{trello_get_token}} for how to obtain it
-#' @param filter limit to just one type of action, e.g. "addMemberToCard"
-#' @param paging whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
-#' @param fix drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
-#' @seealso \code{\link{trello_get_token}}, \code{\link{trello_get}}
+#' @param ... additional arguments passed to \code{\link{trello_get}}
+#' @seealso \code{\link{trello_get}}
 #' @export
 
-get_card_actions = function(id, token = NULL,
-                            filter = NULL,
-                            paging = FALSE, fix = TRUE) {
+get_card_actions = function(id, ...) {
 
     fun = trello_req(parent = "card", child = "actions")
-    dat = fun(id,
-              token = token,
-              query = list(filter = filter),
-              paging = paging, fix = fix)
+    dat = fun(id, ...)
 
     return(dat)
 }
@@ -28,20 +20,14 @@ get_card_actions = function(id, token = NULL,
 #'
 #' Given a card ID, returns a flat \code{data.frame} with comments-related data.
 #' @param id card id
-#' @param token previously generated token, see \code{\link{trello_get_token}} for how to obtain it
-#' @param paging whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
-#' @param fix drop and rename some columns or leave it as is. Defaults to \code{TRUE}
-#' @seealso \code{\link{trello_get_token}}, \code{\link{trello_get}}
+#' @param ... additional arguments passed to \code{\link{trello_get}}
+#' @seealso \code{\link{trello_get}}
 #' @export
 
-get_card_comments = function(id, token = NULL,
-                             paging = FALSE, fix = TRUE) {
+get_card_comments = function(id, ...) {
 
     fun = trello_req(parent = "card", child = "actions")
-    dat = fun(id,
-              token = token,
-              query = list(filter = "commentCard"),
-              paging = paging, fix = fix)
+    dat = fun(id, filter = "commentCard", ...)
 
     return(dat)
 }
@@ -50,21 +36,14 @@ get_card_comments = function(id, token = NULL,
 #'
 #' Given a card ID, returns a flat \code{data.frame} with members-related data.
 #' @param id card id
-#' @param token previously generated token, see \code{\link{trello_get_token}} for how to obtain it
-#' @param paging whether paging should be used (necessary for requests returning more than 1000 rows). Defaults to \code{FALSE}
-#' @param fix drop and rename some columns or leave it as it is. Defaults to \code{TRUE}
-#' @seealso \code{\link{trello_get_token}}, \code{\link{trello_get}}
-#' @importFrom dplyr %>% select
+#' @param ... additional arguments passed to \code{\link{trello_get}}
+#' @seealso \code{\link{trello_get}}
 #' @export
 
-get_card_members = function(id, token = NULL,
-                            paging = FALSE, fix = TRUE) {
+get_card_members = function(id, ...) {
 
     fun = trello_req(parent = "card", child = "members")
-    dat = fun(id,
-              token = token,
-              query = NULL,
-              paging = paging, fix = fix)
+    dat = fun(id, ...)
 
     return(dat)
 }
