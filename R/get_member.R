d2e886807b6c8bfc459f3df5b401530ee0e06e59
Jakub Chromec <chromec.jakub@gmail.com>
2016-08-01 17:07:17 +0200

    call trello_get directly

--- R/get_member.R
+++ R/get_member.R
@@ -3,16 +3,15 @@
 #' Get Own Boards
 #'
 #' Returns a flat \code{data.frame} with all your boards.
-#' @param token previously generated token, see \code{\link{trello_get_token}} for info on how to obtain it
+#' @param token Secure token - get it with \code{\link{trello_get_token}}
 #' @param ... Additional arguments passed to \code{\link{trello_get}}
 #' @seealso \code{\link{trello_get}}
 #' @export
 
 get_my_boards = function(token, ...) {
 
-    fun = trello_req(parent = "member", child = "boards")
-    dat = fun(id = "me", token = token, ...)
-
+    dat = trello_get(parent = "member", child = "boards", id = "me",
+                     token = token, ...)
     return(dat)
 }
 
@@ -26,8 +25,6 @@ get_my_boards = function(token, ...) {
 
 get_member_boards = function(id, ...) {
 
-    fun = trello_req(parent = "member", child = "boards")
-    dat = fun(id, ...)
-
+    dat = trello_get(parent = "member", child = "boards", id = id, ...)
     return(dat)
 }
