831e2ea6c5e819c512a63f51c728739117dffa60
Jakub Chromec <chromec.jakub@gmail.com>
2016-08-01 17:08:13 +0200

    remove github link (vignette is better)

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,7 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
         "R API for Trello\n",
-        "Disclaimer: trellor is not affiliated, associated, authorized, endorsed by
-    or in any way officially connected to Trello, Inc. (www.trello.com).\n",
-        "Visit https://github.com/jchrom/trello for help\n")
+        "Disclaimer: trellor is not affiliated, associated, authorized, endorsed by or in any way officially connected to Trello, Inc. (www.trello.com).\n")
 }
 
 as_POSIXct_hex = function(x) {
