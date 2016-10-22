577ee73659924b5f1bdde1ee4184c7504c5597c8
Jakub Chromec <chromec.jakub@gmail.com>
2016-10-22 17:49:36 +0200

    upd vignettes

---
+++ vignettes/R_API_for_Trello.R
@@ -0,0 +1,35 @@
+## ----setup, include = FALSE----------------------------------------------
+library(httr)
+knitr::opts_chunk$set(comment = "#>", collapse = TRUE)
+
+## ------------------------------------------------------------------------
+library(trelloR)
+url = "https://trello.com/b/nC8QJJoZ/trello-development-roadmap"
+idb = get_id_board(url = url)
+cards = get_board_cards(idb, limit = 5)
+
+## ------------------------------------------------------------------------
+cards
+
+## ------------------------------------------------------------------------
+get_card_updates = function(id, ...) {
+    get_model(parent = "card", child  = "actions", id = id, filter = "updateCard", ...)
+}
+
+## ------------------------------------------------------------------------
+idc = cards$id[1]
+card_updates = get_card_updates(idc, limit = 5)
+
+## ------------------------------------------------------------------------
+board_comments = get_model(parent = "board", child = "actions", id = idb,
+                           filter = "commentCard", limit = 5)
+
+## ------------------------------------------------------------------------
+tryCatch(
+    expr  = get_card_actions(id = "I_have_a_bad_feeling_about_this"),
+    error = function(e) {
+        print(e$message)})
+
+## ------------------------------------------------------------------------
+sessionInfo()
+
