16a989eb47dc1c22b291e9766d5f057d3407481e
Jakub Chromec <chromec.jakub@gmail.com>
2016-12-03 18:51:58 +0100

    add class constructor

--- R/api_s3.R
+++ R/api_s3.R
@@ -5,3 +5,9 @@ add_class = function(x, child) {
     class_api = paste0(child, "_df")
     structure(x, class = c(class_api, "trello_api", class(x)))
 }
+
+# add_attr = function(x, parent, name, id) {
+#     if (!is.data.frame(x)) stop("Not a data frame")
+#     attributes(x)[[parent]] = list(id = id)
+#     x
+# }
