1e1817139925ae4c32317de051b2c72196d6eec1
Jakub Chromec <chromec.jakub@gmail.com>
2016-12-29 10:48:58 +0100

    properly handle S3 classes

--- R/get_pages.R
+++ R/get_pages.R
@@ -14,67 +14,63 @@
 
 get_pages = function(url, token, query = NULL) {
 
-    if (!is.null(query[["limit"]]))
-      limit = query[["limit"]] else limit = 1000
+  if (!is.null(query[["limit"]]))
+    limit = query[["limit"]]
+  else
+    limit = 1000
 
-    # If 0 is supplied for the limit, the value is always set to 1000,
-    # effectively paginating until everything is downloaded.
-    #
-    # For other values of limit, a vector of limits is caculated, e.g.
-    # limit = 2500 yields limit values c(1000, 1000, 500).
-    #
-    # Since paging stops when < 1000 results are returned, limit
-    # values < 1000 will stop it in either case (which is what we want).
+  if (limit < 0)
+    stop("'limit' cannot be negative", call. = FALSE)
 
-    if (limit < 0) {
-        stop("'limit' cannot be negative", call. = FALSE)
-    } else if (limit == 0) {
-        # message("'limit' set to 0: paging will be used")
-        limits = NULL
-    } else if (limit > 0 & limit %% 1000 == 0) {
-        limits = rep(1000, limit/1000)
-    } else {
-        limits = c(rep(1000, trunc(limit/1000)), limit %% 1000)
-    }
+  result = list()
 
-    result = list()
-    page = 0
+  if (limit == 0) {
 
     repeat {
 
-        if (is.null(limits)) {
-            query[["limit"]] = 1000
-            last_page = FALSE
-        } else {
-            page = page + 1
-            query[["limit"]] = limits[page]
-            last_page = length(limits) == page
-        }
+      query[["limit"]] = 1000
+      batch = get_flat(url = url, token = token, query = query)
+      result = append(result, list(batch))
 
-        batch = tryCatch(
-            expr = get_flat(url = url, token = token, query = query),
-            error = function(e) {
-                message("Failed batch: ", e$message)
-                NULL
-            }
-        )
-
-        result = append(result, list(batch))
-
-        if (!is.data.frame(batch)) {
-            message("Cannot determine number of results - stopping...")
-            break
-        } else if (nrow(batch) < 1000 | last_page) {
-            total = ((length(result) - 1) * 1000) + nrow(batch)
-            message("Received ", nrow(batch), " results")
-            message("Request complete: ", total," results")
-            break
-        } else {
-            query$before = batch$id[1000]
-            message("Received 1000 results, keep paging...\n")
-        }
+      if (!is.data.frame(batch)) {
+        message("Result is not a data.frame - stopping...")
+        break
+      } else if (nrow(batch) < 1000) {
+        break
+      } else {
+        query$before = batch$id[1000]
+        message("Received 1000 results\n")
+      }
     }
-    return(result)
+
+  } else {
+
+    limits = c(rep(1000, trunc(limit/1000)), limit %% 1000)
+    limits = limits[limits > 0]
+
+    for (lim in limits) {
+
+      query[["limit"]] = lim
+      batch = get_flat(url = url, token = token, query = query)
+      result = append(result, list(batch))
+
+      if (!is.data.frame(batch)) {
+        message("Result is not a data.frame - stopping...")
+        break
+      } else if (nrow(batch) < 1000) {
+        break
+      } else {
+        query$before = batch$id[1000]
+        message("Received 1000 results\n")
+      }
+    }
+  }
+
+  total = ((length(result) - 1) * 1000) + nrow(batch)
+  message("Received ", nrow(batch), " results")
+  message("Request complete: ", total," results")
+
+  return(result)
 }
 
 # url = "https://api.trello.com/1/board/54212b5181d0b59cfbff6de0/cards"
