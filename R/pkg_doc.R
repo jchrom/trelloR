413a6d34fe379c7fdde985363b1fdb5f899e0603
Jakub Chromec <chromec.jakub@gmail.com>
2016-12-31 12:54:11 +0100

    .

---
+++ R/pkg_doc.R
@@ -0,0 +1,9 @@
+#' trelloR: R API for Trello
+#'
+#' Currently contains get_ and search_ functions for authorizing access to your
+#' private boards and getting data. Summary and plotting functions will be
+#' implemented in the future (watch \url{https://github.com/jchrom/trelloR}).
+#'
+#' @docType package
+#' @name foo
+NULL
