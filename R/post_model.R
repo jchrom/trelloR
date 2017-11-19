2512bb940601bdc3f1744e299084dc62f13abe4f
Jakub Chromec <chromec.jakub@gmail.com>
2017-11-19 14:50:04 +0000

    update docs

--- R/post_model.R
+++ R/post_model.R
@@ -10,8 +10,8 @@
 #' @param body A named list of query paramters (will be passed as body)
 #' @param token Secure token, see \code{\link{get_token}} (scope must include write permissions)
 #' @param verbose Whether to pass \code{verbose()} to \code{\link[httr]{POST}}
-#' @param response Can return \code{"content"} (default), \code{"headers"} or the complete \code{"response"}
-#' @param on.error Issues either \code{\link[base]{warning}} (default), \code{\link[base]{message}} or \code{\link[base]{stop}}s
+#' @param response Can return \code{"content"} (default), \code{"headers"}, \code{"status"} code or the complete \code{"response"}
+#' @param on.error Issues either \code{\link[base]{warning}} (default), \code{\link[base]{message}} or error (and \code{\link[base]{stop}}s)
 #' @param ... Additional arguments passed to \code{\link[httr]{POST}}
 #' @importFrom httr modify_url POST content status_code headers message_for_status warn_for_status stop_for_status
 #' @export
