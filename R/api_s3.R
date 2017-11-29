52215fa5260fc82f3e7c2792274165c9b33cb548
Jakub Chromec <chromec.jakub@gmail.com>
2017-11-29 23:14:39 +0000

    streamline add class

--- R/api_s3.R
+++ R/api_s3.R
@@ -1,9 +1,18 @@
 # Class constructor
 
 add_class = function(x, child) {
-  if (!is.data.frame(x)) stop("Not a data frame")
-  class_api = paste0(child, "_df")
-  structure(x, class = c(class_api, "trello_api", class(x)))
+
+  stopifnot(is.data.frame(x))
+
+  if (!is.null(child))
+
+    structure(
+      x, class = c(paste0(child, "_df"), "trello_api", class(x))
+    )
+
+  else
+
+    x
 }
 
 #' Trello API objects
