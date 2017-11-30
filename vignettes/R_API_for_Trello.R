3927f4f74839b54d1c24fe2e621231a4880e53ff
Jakub Chromec <chromec.jakub@gmail.com>
2017-11-30 00:21:36 +0000

    minor edits

--- vignettes/R_API_for_Trello.R
+++ vignettes/R_API_for_Trello.R
@@ -2,7 +2,7 @@
 library(httr)
 knitr::opts_chunk$set(comment = "#>", collapse = TRUE)
 
-## ------------------------------------------------------------------------
+## ---- results='hide'-----------------------------------------------------
 library(trelloR)
 url = "https://trello.com/b/nC8QJJoZ/trello-development-roadmap"
 idb = get_id_board(url = url)
@@ -12,22 +12,22 @@ cards = get_board_cards(idb, limit = 5)
 cards
 
 ## ------------------------------------------------------------------------
-head(dplyr::as.tbl(cards))
+cards[1:5, c("name", "closed", "shortUrl")]
 
 ## ------------------------------------------------------------------------
 get_card_updates = function(id, ...) {
     get_model(parent = "card", child  = "actions", id = id, filter = "updateCard", ...)
 }
 
-## ------------------------------------------------------------------------
+## ---- results='hide'-----------------------------------------------------
 idc = cards$id[1]
 card_updates = get_card_updates(idc, limit = 5)
 
-## ------------------------------------------------------------------------
+## ---- results='hide'-----------------------------------------------------
 board_comments = get_model(parent = "board", child = "actions", id = idb,
                            filter = "commentCard", limit = 5)
 
-## ------------------------------------------------------------------------
+## ---- results='hide'-----------------------------------------------------
 tryCatch(
     expr  = get_card_actions(id = "I_have_a_bad_feeling_about_this"),
     error = function(e) message(e$message)
